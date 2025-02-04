fb77a88675659efeb055c6ec72526225419fbe61
nazwaoktavia <nazwaoktaviasalsabila28@gmail.com>
2025-02-04 09:57:59 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Set view engine ke EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware untuk menyajikan file statis
+app.use(express.static(path.join(__dirname, "public")));
+
+// Set view engine ke EJS
+app.set("view engine", "ejs");
+
+// Rute halaman utama
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// Rute halaman login
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+// Rute halaman register
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.get("/home", (req, res) => {
+    res.render("home");
+});
+
+// Jalankan server
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+});
